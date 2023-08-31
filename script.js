d1ef0bf2c7ac0de53b1742f2c530da97889c2193
Jemmy09 <134119565+Jemmy09@users.noreply.github.com>
2023-08-31 13:48:07 +0800

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-// Example: Scroll to section smoothly when clicking navigation links
-document.querySelectorAll('nav a').forEach(link => {
+// Get the navigation links and sections
+const navLinks = document.querySelectorAll('nav a');
+const sections = document.querySelectorAll('section');
+
+// Smooth scroll when clicking on navigation links
+navLinks.forEach(link => {
     link.addEventListener('click', event => {
         event.preventDefault();
-        const target = document.querySelector(link.getAttribute('href'));
-        target.scrollIntoView({ behavior: 'smooth' });
+        const targetSection = document.querySelector(link.getAttribute('href'));
+        targetSection.scrollIntoView({ behavior: 'smooth' });
     });
+});
+
+// Change the header background color on scroll
+const header = document.querySelector('header');
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
 });
