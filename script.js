a35b7eaebc312d6eed38cdbd5b224cbafb9881c7
unknown <jemmyfrancisco30@gmail.com>
2023-08-31 11:54:50 +0800

    firstcommit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Example: Scroll to section smoothly when clicking navigation links
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', event => {
+        event.preventDefault();
+        const target = document.querySelector(link.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth' });
+    });
+});
