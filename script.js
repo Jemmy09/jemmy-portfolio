91b0d50708e579fa43998b9ef3b368badec37ded
Jemmy Francisco <134119565+Jemmy09@users.noreply.github.com>
2025-08-02 02:57:32 +0800

    script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 // Smooth scroll
 document.querySelectorAll('nav a').forEach(link => {
-    link.addEventListener('click', event => {
-        event.preventDefault();
-        const target = document.querySelector(link.getAttribute('href'));
-        target.scrollIntoView({ behavior: 'smooth' });
-    });
+  link.addEventListener('click', event => {
+    event.preventDefault();
+    const target = document.querySelector(link.getAttribute('href'));
+    target.scrollIntoView({ behavior: 'smooth' });
+  });
 });
 
-// Scroll effect on header
+// Header scroll effect
 const header = document.querySelector('header');
 window.addEventListener('scroll', () => {
-    if (window.scrollY > 50) {
-        header.classList.add('scrolled');
-    } else {
-        header.classList.remove('scrolled');
-    }
+  if (window.scrollY > 50) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
 });
